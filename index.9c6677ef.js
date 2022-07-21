e99fdf953ed61878330c37e195200dbddf83928c
MichaelNovikov95 <100936797+MichaelNovikov95@users.noreply.github.com>
2022-07-21 15:51:12 +0000

    Deploying to gh-pages from @ MichaelNovikov95/Filmoteka@19e41a62088aeceb1f0b3765f2a69f3720562725 🚀

---
+++ index.9c6677ef.js
@@ -0,0 +1,2 @@
+document.querySelector(".footer__link").addEventListener("click",(function(){}));
+//# sourceMappingURL=index.9c6677ef.js.map
---
+++ index.cb9b3b6d.js
@@ -0,0 +1,2 @@
+document.querySelector(".footer__link").addEventListener("click",(function(){}));
+//# sourceMappingURL=index.cb9b3b6d.js.map
