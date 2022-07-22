a913736929caed7d46c9eb6cab99d6dc234fe486
BondAndrii <CitrusOk777@gmail.com>
2022-07-22 22:15:26 +0300

    add action "close modal window"

--- src/script/onOpenCard.js
+++ src/script/onOpenCard.js
@@ -3,7 +3,9 @@ import { movieCard } from './movieCard';
 const modal = document.querySelector('.backdrop');
 const galleryEl = document.querySelector('.gallery');
 const movieApi = new MovieApi();
-
+// ---------------------------------------------------------------------
+let closeModalFilmBtn = null;
+// ---------------------------------------------------------------------
 const createMarkup = async id => {
   clearCard();
   movieApi.id = id;
@@ -11,6 +13,11 @@ const createMarkup = async id => {
     const { data } = await movieApi.fetchMovieById();
     console.log(data);
     modal.insertAdjacentHTML('beforeend', movieCard(data));
+    // ---------------------------------------------------------------------
+    const closeModalFilmBtn = document.querySelector(".close__button");
+    console.log(closeModalFilmBtn);
+    closeModalFilmBtn.addEventListener('click', closeModal);
+    // ---------------------------------------------------------------------
   } catch (err) {
     console.log(err);
   }
@@ -21,11 +28,20 @@ const onGalleryContainerClick = e => {
   if (e.target.nodeName !== 'IMG') {
     return;
   }
-  // modal.classList.remove('is-hidden'); добавь display:none на этот класс чтоб скрыть модалку
-  createMarkup(e.target.id);
+  modal.classList.remove('is-hidden'); 
+  createMarkup(e.target.id);  
 };
 
 function clearCard() {
   modal.innerHTML = '';
 }
 galleryEl.addEventListener('click', onGalleryContainerClick);
+
+// ---------------------------------------------------------------------
+
+function closeModal() {
+  modal.classList.add('is-hidden');
+  modal.innerHTML = '';
+  // closeModalFilmBtn.removeEventListener('click', closeModal);
+}
+// ---------------------------------------------------------------------
