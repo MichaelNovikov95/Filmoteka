b4fbae413051e9c2c52178773030b48240af84a9
Vlad Shchelkunov <specter.ode@gmail.com>
2022-07-22 13:53:46 +0300

    oneCard full info
    при нажатии на карточку подтягивается вся информация с базы данных о фильме

---
+++ src/script/movieCard.js
@@ -0,0 +1,53 @@
+export const movieCard = ({
+  id,
+  title,
+  poster_path,
+  popularity,
+  original_title,
+  vote_average,
+  vote_count,
+  genres,
+  overview,
+}) => {
+  poster_path
+    ? (poster_path = `https://image.tmdb.org/t/p/w500/${poster_path}`)
+    : (poster_path = '../images/header/search.svg');
+  // на 13 строке нужно поставить картинку заглушку
+  genres = genres.map(genre => genre.name).join(', ');
+  popularity = parseFloat(popularity).toFixed(1);
+
+  return `
+  <div class="modal">
+    <button class="close__button"></button>
+    <div class="modal__container">
+      <div class="image__place" id=${id}>
+        <img src="${poster_path}" alt="${original_title}" />
+      </div>
+      <div class="content__place">
+          <h2 class="modal__header">${title}</h2>
+          <div class="details">
+              <ul class="details-head">
+                  <li class="details-string">Vote / Votes</li>
+                  <li class="details-string">Popularity</li>
+                  <li class="details-string">Original Title</li>
+                  <li class="details-string">Genre</li>
+              </ul>
+              <ul class="details-content">
+                  <li class="details-string"><span class="vote--modal">${vote_average}</span> / ${vote_count}</li>
+                  <li class="details-string">${popularity}</li>
+                  <li class="details-string">${original_title}</li>
+                  <li class="details-string">${genres}</li>
+              </ul>
+          </div>
+          <h3>ABOUT</h3>
+          <p class="modal-annotation">
+              ${overview}
+          </p>
+          <ul class="button__place">
+              <button type="button" class="film-modal__button film-modal__button--active">ADD TO WATCHED</button>
+              <button type="button" class="film-modal__button">ADD TO QUEUE</button>
+          </ul>
+      </div>
+    </div>
+  </div>`;
+};
